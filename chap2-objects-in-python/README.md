5300dfc6b5e71570399244ee25edaa2ea0038faf
Duc Le <dukele35@gmail.com>
2021-12-18 18:41:32 +0700

    classes info

--- chap2-objects-in-python/README.md
+++ chap2-objects-in-python/README.md
@@ -2,4 +2,19 @@
 
 ## 1. The usage of mypy package ##
 
-The [mypy](https://mypy.readthedocs.io/en/stable/getting_started.html) package helps us to detect erros in our python code where [2.1.mypy-package-usage.py](https://github.com/dukele35/python-oop-practices/blob/main/chap2-objects-in-python/mypy_usage.py) is an example
+- The [mypy](https://mypy.readthedocs.io/en/stable/getting_started.html) package helps us to detect static errors in our python code where [mypy_usage.py](https://github.com/dukele35/python-oop-practices/blob/main/chap2-objects-in-python/mypy_usage.py) is an example.
+- the usage: using `mypy --strict <python_file.py>` in the terminal.
+- [Example](http://mypy-lang.org/).
+
+## 2. Notices regarding Python Class ##
+
+- For this, please refer to [points.py](https://github.com/dukele35/python-oop-practices/blob/main/chap2-objects-in-python/points.py).
+- Docstring is essential for writing python classes, methods, functions, etc. Please refer to the python file above as the blue-print or conventions for writing python doctring. 
+- To enter the interactive shell with one particular python file, please use `python -i <python_file.py>` in the terminal. You are then able to get the information of the class by typing `help(<Class_Name>)` in the interactive environment. You can also get the information of the class or an instance with `<Instance>.__doc__`. 
+- It is advisable to include example in the docstring with the following format:
+```
+>>> <something_1>
+>>> <something_2>
+result 
+```
+- Such examples could be validated by `python -m doctest -v <python_file.py>` in the terminal.
--- chap2-objects-in-python/points.py
+++ chap2-objects-in-python/points.py
@@ -1,13 +1,76 @@
-import math 
+import math
 
 class Point:
+    """
+    A class used to represent a point
+    ...
+    Attributes
+    ----------
+    x (float): a float x-coordinate 
+    y (float): a float y-coordinate 
 
-    def move(self, x:float, y:float) -> None:
+    Methods
+    ----------
+    move(x,y): 
+        Move a point to a given coordinates of x & y
+    reset(): 
+        Reset a point's coordinates to x = 0 & y = 0.
+    calculate_distance(Point(x,y)): 
+        Calculate Euclidean distance between the current point with
+        another point as a parameter.
+
+    Examples
+    ---------
+    >>> p_0 = Point()
+    >>> p_1 = Point(3, 4)
+    >>> p_0.calculate_distance(p_1)
+    5.0
+    """
+
+    def __init__(self,x:float=0,y:float=0) -> None:
+        """
+        Assign x coordinate and y coordinate for one sinple point.
+        Parameters:
+            x (float): a float x-coordinate 
+            y (float): a float y-coordinate
+        """
+        self.move(x,y)
+
+    def move(self,x:float,y:float) -> None:
+        """
+        Move a point to a given coordinates of x & y.
+        Parameters:
+            x (float): a float x-coordinate 
+            y (float): a float y-coordinate
+        """
         self.x = x
         self.y = y
 
     def reset(self) -> None:
-        self.move(0, 0)
+        """
+        Reset a point's coordinates to x = 0 & y = 0.
+        """
+        self.move(0,0)
 
     def calculate_distance(self, other: "Point") -> float:
+        """
+        Calculate Euclidean distance between the current point with
+        another point as a parameter. 
+        Parameters:
+            other: a Point instance
+        Returns:
+            an Euclidean distance (float)
+        """
         return math.hypot(self.x - other.x, self.y - other.y)
+
+def main() -> None:
+    """
+    >>> main()
+    5.0
+    """
+    point1 = Point()
+    point2 = Point(3,4)
+    print(f"{point1.calculate_distance(point2)}")
+
+if __name__ == "__main__":
+    main()
