1206031673dfcefb940a05296e4d53e2993522c2
Duc Le <dukele35@gmail.com>
2021-12-21 01:39:05 +0700

    Optional

--- chap2-objects-in-python/typing_usage.py
+++ chap2-objects-in-python/typing_usage.py
@@ -2,18 +2,20 @@ from typing import  List, Dict, Set, \
                     Optional, Any, Sequence, \
                     Tuple, Callable, TypeVar, Union
 
-# 1 - List
+##### 1 - List #####
 x1 : List[int]              = [1,2,3]               # valid
 x2 : List[int,str]          = [1,2,3,"a"]           # invalid --> only expecting one argument
 x3 : List[Union[int,str]]   = [1,2,3,"a"]           # valid
 x4 : List[List[int]]        = [[1,2,3],[3,4],[9]]   # valid
 
 
-# 2 - Dict
+
+##### 2 - Dict #####
 y1 : Dict[str,str]          = {"a":"b"}             # valid
 y2 : Dict[str,str,int]      = {"a":"b"}             # invalid --> only expecting two arguments
 y3 : Dict[str,int]          = {"a":3}               # valid
 y4 : Dict[int,str]          = {"a":3}               # invalid --> incompatible types
+
 
 
 # 3 - Set
@@ -21,17 +23,29 @@ z1 : Set[str]               = {"a","b"}             # valid
 z2 : Set[str,int]           = {"a",3}               # invalid --> only expecting one argument
 z3 : Set[Union[str,int]]    = {"a",3}               # valid
 
-# 4 - Custom type by defining a new datatype 
+
+
+##### 4 - Custom type by defining a new datatype ##### 
 # NB. this is very useful for 
 # defining complicated datatype scenario
 Vector = List[float]                                # valid
 
-def foo(v:Vector) -> Vector:                        # valid
+def f1(v:Vector) -> Vector:                         # valid
     return [i/4 for i in v]
 
-print(foo([1,2,3]))                                 # valid
+print (f1([1,2,3]))                                 # valid
 
-# 5 - Any
+
+
+##### 5 - Optional #####
+def f2(xyz: Optional[bool] = False) -> int:         # valid          
+    return 3
+
+print (f2(False))                                   # valid
+print (f2())                                        # valid
+
+
+
 
 # 6 - Sequence
 
