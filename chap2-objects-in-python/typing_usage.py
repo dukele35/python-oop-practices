c98b4f9061fc59aad137836fcddcc2e5d7852267
Duc Le <dukele35@gmail.com>
2021-12-21 01:47:51 +0700

    Any

--- chap2-objects-in-python/typing_usage.py
+++ chap2-objects-in-python/typing_usage.py
@@ -2,15 +2,18 @@ from typing import  List, Dict, Set, \
                     Optional, Any, Sequence, \
                     Tuple, Callable, TypeVar, Union
 
+####################
 ##### 1 - List #####
+####################
 x1 : List[int]              = [1,2,3]               # valid
 x2 : List[int,str]          = [1,2,3,"a"]           # invalid --> only expecting one argument
 x3 : List[Union[int,str]]   = [1,2,3,"a"]           # valid
 x4 : List[List[int]]        = [[1,2,3],[3,4],[9]]   # valid
 
 
-
+####################
 ##### 2 - Dict #####
+####################
 y1 : Dict[str,str]          = {"a":"b"}             # valid
 y2 : Dict[str,str,int]      = {"a":"b"}             # invalid --> only expecting two arguments
 y3 : Dict[str,int]          = {"a":3}               # valid
@@ -18,14 +21,17 @@ y4 : Dict[int,str]          = {"a":3}               # invalid --> incompatible t
 
 
 
-# 3 - Set
+###################
+##### 3 - Set #####
+###################
 z1 : Set[str]               = {"a","b"}             # valid
 z2 : Set[str,int]           = {"a",3}               # invalid --> only expecting one argument
 z3 : Set[Union[str,int]]    = {"a",3}               # valid
 
 
-
-##### 4 - Custom type by defining a new datatype ##### 
+######################################################
+##### 4 - Custom type by defining a new datatype #####
+######################################################
 # NB. this is very useful for 
 # defining complicated datatype scenario
 Vector = List[float]                                # valid
@@ -36,8 +42,9 @@ def f1(v:Vector) -> Vector:                         # valid
 print (f1([1,2,3]))                                 # valid
 
 
-
+########################
 ##### 5 - Optional #####
+########################
 def f2(xyz: Optional[bool] = False) -> int:         # valid          
     return 3
 
@@ -45,7 +52,15 @@ print (f2(False))                                   # valid
 print (f2())                                        # valid
 
 
+###################
+##### 6 - Any #####
+###################
+def f3(abc: Any) -> str:                            # valid          
+    return "this accepts any types"
 
+print (f3(False))                                   # valid
+print (f3(456))                                     # valid
+print (f3("love this"))                             # valid
 
 # 6 - Sequence
 
