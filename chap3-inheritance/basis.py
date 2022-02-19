8ea24c4c10242d082efe9fc37fa375aabf8ba69b
Duc Le <dukele35@gmail.com>
2022-02-20 00:19:47 +0700

    comments

--- chap3-inheritance/basis.py
+++ chap3-inheritance/basis.py
@@ -24,12 +24,17 @@ class Supplier(Contact):
     """
     Supplier class inherits all of attributes and methods from Contact class.
     Thus, initiating a new object under Supplier class is similar to Contact class.
+
+    Examples
+    --------
     >>> s1 = Supplier("sup1", "sup1@email.com")
     >>> s2 = Supplier("sup2", "sup2@email.com")
     >>> print (Supplier.all_contacts)
     [Contact(duke, duke@email.com), Contact(mike, mike@email.com), Supplier(sup1, sup1@email.com), Supplier(sup2, sup2@email.com)]
+
     >>> s1.order(order='Apple')
     If this were a real system we would send 'Apple' order to 'sup1'
+
     >>> s2.order(order='Orange')
     If this were a real system we would send 'Orange' order to 'sup2'
     """
